be47ba8d00a00fe03dfc5ef315443de42cf8d86f
nsbasalai <nsbasalai@gmail.com>
2022-10-25 13:38:53 +0300

    fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-async function getDashboardData(url = 'https://github.com/nsbasalai/dashboard/blob/main/data.jsonlai.github.io/data.json') {
+async function getDashboardData(url = 'https://github.com/nsbasalai/dashboard/blob/main/data.json') {
     const response = await fetch(url);
     const data = await response.json();
 
